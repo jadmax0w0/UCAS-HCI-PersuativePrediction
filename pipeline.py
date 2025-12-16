220d2b1a9f714bfc97f2e02b7a03ae65f75c6625
jadmax0w0 <jadmax@qq.com>
2025-12-16 23:08:41 +0800

    Refactor training and evaluation data processing into _get_dataset_features function for improved code organization and reusability

--- pipeline.py
+++ pipeline.py
@@ -58,6 +58,44 @@ def parse_cmv_data(rawdata: list[dict], max_char_count: Optional[int] = None, ma
     return pd.DataFrame(samples, columns=['o', 'p', 'label'])
 
 
+def _get_dataset_features(
+        partition: Literal['train', 'val'],
+        data_path: Optional[str],
+        cialdini_extractor: Union[CialdiniFeatureExtractor, str],
+        dim10_extractor: Dim10FeatureExtractor,
+        bert_extractor: BertTextFeatureExtractor,
+):
+    """数据集转特征 & 拼接"""
+    texts = load_cmv_data(data_path)
+    df = parse_cmv_data(texts, max_char_count=100, max_data_count=300)  # TODO: 上量
+
+    # Cialdini feats
+    if isinstance(cialdini_extractor, CialdiniFeatureExtractor):
+        p_feat_cialdini = cialdini_extractor.extract(df["p"].values.tolist())
+    else:
+        p_feat_p, p_feat_n = Cialdini.load_from_anno_file(cialdini_extractor, partition=partition)
+        assert p_feat_p.shape == p_feat_n.shape, f"{p_feat_p.shape=}, {p_feat_n.shape=}"
+        p_feat_cialdini = np.concat([p_feat_p, p_feat_n], axis=-1).reshape(-1, p_feat_p.shape[1])
+        p_feat_cialdini = p_feat_cialdini[:len(df)]
+    
+    # 10 dim feats
+    if partition == 'train' and not dim10_extractor.trained():
+        dim10_extractor.train()
+    p_feat_dim10 = dim10_extractor.extract(df["p"].values.tolist())
+    
+    # Bert feats
+    if partition == 'train' and not bert_extractor.trained():
+        bert_extractor.train()
+    p_feat_bert = bert_extractor.extract(df["p"].values.tolist())
+    o_feat_bert = bert_extractor.extract(df["o"].values.tolist())
+
+    # Concat feats
+    x = np.concat([p_feat_cialdini, p_feat_dim10, p_feat_bert, o_feat_bert], axis=-1)
+
+    y = df['label'].values.astype(int)
+    return x, y
+
+
 def train_model(
         model: BaseClassifier,
         data_path: Optional[str],
@@ -74,49 +112,27 @@ def train_model(
     Returns:
         tuple: (trained model, trained feat. extractors)
     """
-    ## Load training data
-    print("Load training dataset")
+    ## Load and vectorize training data
+    print(">>> Load and vectorize training dataset")
     data_path = "./dataset/train.jsonl" if data_path is None else data_path
-    texts_train = load_cmv_data(data_path)
-    df_train = parse_cmv_data(texts_train, max_char_count=100, max_data_count=300)  # TODO: 上量
-
-    ## Get text features
-    print("Prepare features")
-    # Cialdini feats
-    if isinstance(cialdini_extractor, CialdiniFeatureExtractor):
-        p_feat_cialdini = cialdini_extractor.extract(df_train["p"].values.tolist())
-    else:
-        p_feat_p, p_feat_n = Cialdini.load_from_anno_file(cialdini_extractor, partition='train')
-        assert p_feat_p.shape == p_feat_n.shape, f"{p_feat_p.shape=}, {p_feat_n.shape=}"
-        p_feat_cialdini = np.concat([p_feat_p, p_feat_n], axis=-1).reshape(-1, p_feat_p.shape[1])
-        p_feat_cialdini = p_feat_cialdini[:len(df_train)]
-    
-    # 10 dim feats
-    if not dim10_extractor.trained():
-        dim10_extractor.train()
-    p_feat_dim10 = dim10_extractor.extract(df_train["p"].values.tolist())
-    
-    # Bert feats
-    if not bert_extractor.trained():
-        bert_extractor.train()
-    p_feat_bert = bert_extractor.extract(df_train["p"].values.tolist())
-    o_feat_bert = bert_extractor.extract(df_train["o"].values.tolist())
-
-    # Concat feats
-    x_train = np.concat([p_feat_cialdini, p_feat_dim10, p_feat_bert, o_feat_bert], axis=-1)
-
-    y_train = df_train['label'].values.astype(int)
-    print(f"Feature extracted - x {x_train.shape}, y {y_train.shape}")
+    x_train, y_train = _get_dataset_features(
+        partition='train',
+        data_path=data_path,
+        cialdini_extractor=cialdini_extractor,
+        dim10_extractor=dim10_extractor,
+        bert_extractor=bert_extractor,
+    )
+    print(f">>> Feature extracted - x {x_train.shape}, y {y_train.shape}")
 
     ## Train classification model
-    print("Train model")
+    print(">>> Train model")
     model.train(x_train, y_train)
 
     if model_save_path is not None:
         save_model(model, model_save_path)
-        print(f"Model saved at {model_save_path}")
+        print(f">>> Model saved at {model_save_path}")
     
-    print("Train complete")
+    print(">>> Train complete")
     return model
 
 
@@ -136,43 +152,25 @@ def eval_model(
         确保 `feat_extractors` 是 `train_model()` 中训练过的那些 & 确保 `o_p_integrate_method` 与 `train_model()` 一致
     """
     if isinstance(model_path, str):
-        print("Load model checkpoint")
+        print(">>> Load model checkpoint")
         model = load_model(model_path, return_only_model=True)
     else:
         model = model_path
 
-    ## Load eval data
-    print("Load evaluation dataset")
+    ## Load and vectorize eval data
+    print(">>> Load and vectorize evaluation dataset")
     data_path = "./dataset/val.jsonl" if data_path is None else data_path
-    texts_test = load_cmv_data(data_path)
-    df_test = parse_cmv_data(texts_test, max_char_count=100, max_data_count=300)
-
-    # Get features
-    print("Prepare features")
-    # Cialdini feats
-    if isinstance(cialdini_extractor, CialdiniFeatureExtractor):
-        p_feat_cialdini = cialdini_extractor.extract(df_test["p"].values.tolist())
-    else:
-        p_feat_p, p_feat_n = Cialdini.load_from_anno_file(cialdini_extractor, partition='val')
-        assert p_feat_p.shape == p_feat_n.shape, f"{p_feat_p.shape=}, {p_feat_n.shape=}"
-        p_feat_cialdini = np.concat([p_feat_p, p_feat_n], axis=-1).reshape(-1, p_feat_p.shape[1])
-        p_feat_cialdini = p_feat_cialdini[:len(df_test)]
-    
-    # 10 dim feats
-    p_feat_dim10 = dim10_extractor.extract(df_test["p"].values.tolist())
-    
-    # Bert feats
-    p_feat_bert = bert_extractor.extract(df_test["p"].values.tolist())
-    o_feat_bert = bert_extractor.extract(df_test["o"].values.tolist())
-
-    # Concat feats
-    x_test = np.concat([p_feat_cialdini, p_feat_dim10, p_feat_bert, o_feat_bert], axis=-1)
-
-    y_test = df_test['label'].values.astype(int)
-    print(f"Feature extracted - x {x_test.shape}, y {y_test.shape}")
+    x_test, y_test = _get_dataset_features(
+        partition='val',
+        data_path=data_path,
+        cialdini_extractor=cialdini_extractor,
+        dim10_extractor=dim10_extractor,
+        bert_extractor=bert_extractor,
+    )
+    print(f">>> Feature extracted - x {x_test.shape}, y {y_test.shape}")
 
     ## Prediction
-    print("Model predict")
+    print(">>> Model predict")
     y_pred = model.predict(x_test)
 
     print(classification_report(y_test, y_pred))
